65a5bc6f5a509ba2a6f4c7b54a34db409ff64e0f
Natalie <naspen@naspen.mov>
2024-07-10 07:19:32 -0400

    SHIELD WALL
    Add shields for projects on the wiki

--- src/theme/DocItem/Content/index.js
+++ src/theme/DocItem/Content/index.js
@@ -38,6 +38,8 @@ export default function DocItemContent({children}) {
   const {tags} = metadata;
   const canDisplayTagsRow = tags.length > 0;
 
+  const myOrange = 'ce7738';
+
   return (
     <div className={clsx(ThemeClassNames.docs.docMarkdown, 'markdown')}>
 
@@ -54,26 +56,30 @@ export default function DocItemContent({children}) {
                   <Heading as="h1">{frontMatter.title} </Heading>
                 </header>
                 <Row>
-                  <Col width="3">
-                    {frontMatter.github_link && (
-                        <Card
-                        iconPath="/icons/github-white.svg"
-                        title="GitHub Repo"
-                        href={frontMatter.github_link}
-                        description=""
-                      />
-                    )}
-                  </Col>
-                  <Col width="3">
-                    {frontMatter.modrinth_link && (
-                        <Card
-                        iconPath="/icons/modrinth.svg"
-                        title="Modrinth Page"
-                        href={frontMatter.modrinth_link}
-                        description=""
-                      />
-                    )}
-                  </Col>
+                  {frontMatter.github_link && frontMatter.github_repo && (
+                    <>
+                    <div className={'margin-horiz--xs'}>
+                      <a href={frontMatter.github_link}>
+                        <img alt="GitHub" src={`https://img.shields.io/github/created-at/theNaspenGrove/${frontMatter.github_repo}?style=flat-square&label=GitHub&logo=github&color=${myOrange}`}/>
+                      </a>
+                    </div>
+                    <div className={'margin-horiz--xs'}>
+                    <img alt="GitHub last commit" src={`https://img.shields.io/github/last-commit/theNaspenGrove/${frontMatter.github_repo}?style=flat-square&label=Last%20Commit&logo=github&color=${myOrange}`}/>
+                    </div>
+                  </>
+                  )}
+                  {frontMatter.modrinth_link && frontMatter.modrinth_project_id && (
+                    <>
+                    <div className={'margin-horiz--xs'}>
+                      <a href={frontMatter.modrinth_link} >
+                        <img alt="Modrinth Downloads" src={`https://img.shields.io/modrinth/v/${frontMatter.modrinth_project_id}?logo=modrinth&label=Modrinth&style=flat-square&color=${myOrange}`}/>
+                      </a>
+                    </div>
+                    <div className={'margin-horiz--xs'}>
+                      <img alt="Supported Game Versions" src={`https://img.shields.io/modrinth/game-versions/${frontMatter.modrinth_project_id}?style=flat-square&label=Supported%20Verions&logo=data%3Aimage%2Fsvg%2Bxml%3Bbase64%2CPD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8%2BDQo8IS0tIFVwbG9hZGVkIHRvOiBTVkcgUmVwbywgd3d3LnN2Z3JlcG8uY29tLCBHZW5lcmF0b3I6IFNWRyBSZXBvIE1peGVyIFRvb2xzIC0tPg0KPHN2ZyB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9IjAgMCAxMiAxMiIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj4NCiAgICANCiAgICA8dGl0bGU%2BZW1vamlfbWluZWNyYWZ0X3NpbXBsZSBbIzQ2NF08L3RpdGxlPg0KICAgIDxkZXNjPkNyZWF0ZWQgd2l0aCBTa2V0Y2guPC9kZXNjPg0KICAgIDxkZWZzPg0KDQo8L2RlZnM%2BDQogICAgPGcgaWQ9IlBhZ2UtMSIgc3Ryb2tlPSJub25lIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI%2BDQogICAgICAgIDxnIGlkPSJEcmliYmJsZS1MaWdodC1QcmV2aWV3IiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtMjI0LjAwMDAwMCwgLTYxMjcuMDAwMDAwKSIgZmlsbD0iI0ZGRkZGRiI%2BDQogICAgICAgICAgICA8ZyBpZD0iaWNvbnMiIHRyYW5zZm9ybT0idHJhbnNsYXRlKDU2LjAwMDAwMCwgMTYwLjAwMDAwMCkiPg0KICAgICAgICAgICAgICAgIDxwYXRoIGQ9Ik0xNzIsNTk3MyBMMTcwLDU5NzMgTDE3MCw1OTc5IEwxNzIsNTk3OSBMMTcyLDU5NzcgTDE3Niw1OTc3IEwxNzYsNTk3OSBMMTc4LDU5NzkgTDE3OCw1OTczIEwxNzYsNTk3MyBMMTc2LDU5NzEgTDE3Miw1OTcxIEwxNzIsNTk3MyBaIE0xNzYsNTk3MSBMMTgwLDU5NzEgTDE4MCw1OTY3IEwxNzYsNTk2NyBMMTc2LDU5NzEgWiBNMTY4LDU5NzEgTDE3Miw1OTcxIEwxNzIsNTk2NyBMMTY4LDU5NjcgTDE2OCw1OTcxIFoiIGlkPSJlbW9qaV9taW5lY3JhZnRfc2ltcGxlLVsjNDY0XSI%2BDQoNCjwvcGF0aD4NCiAgICAgICAgICAgIDwvZz4NCiAgICAgICAgPC9nPg0KICAgIDwvZz4NCjwvc3ZnPg%3D%3D&color=${myOrange}`}/>
+                    </div>
+                    </>
+                  )}
                 </Row>
                 {canDisplayTagsRow && (
                   <TagsListInline tags={tags} />
